8a865e9355995eecd5587d599f4cba039c123d28
Sandeep <sandeep@opstree.com>
2020-06-07 19:20:35 +0530

    fixed variable name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nat_ip" {
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat_ip.id
-  subnet_id     = element(var.subnet_for_nat_gw,1)
+  subnet_id     = element(var.subnets_for_nat_gw,1)
   tags = merge(
     {
       Name = format("%s-nat", var.vpc_name)
