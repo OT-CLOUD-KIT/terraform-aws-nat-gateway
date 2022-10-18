168187e8c3e021aa18721573a00c85ba00a6ab33
pnikk7 <nikhil.panchal@opstree.com>
2022-10-18 10:10:39 +0530

    adding multi az support in nat

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "nat_ip" {
+  count = var.enable_az_specific == true ? length(var.subnets_for_nat_gw) : 1
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  count = var.enable_az_specific == true ? length(var.subnets_for_nat_gw) : 1
+  allocation_id = aws_eip.nat_ip.*.id[count.index]
+  subnet_id     = var.subnets_id[count.index]
+  tags = merge(
+    {
+      Name = format("%s", var.name)
+    },
+    var.tags,
+  )
+}
