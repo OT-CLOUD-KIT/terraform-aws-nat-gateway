ebdd97742d84ac82dd1cb68c9d8922b3b16b92f6
Nikhil Panchal <nikhil.panchal@opstree.com>
2022-11-02 14:55:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_eip" "nat_ip" {
-  count = var.enable_az_specific == true ? length(var.subnets_for_nat_gw) : 1
+  count = var.enable_az_specific == true ? length(var.subnets_id) : 1
   vpc = true
 }
 
 resource "aws_nat_gateway" "nat-gw" {
-  count = var.enable_az_specific == true ? length(var.subnets_for_nat_gw) : 1
+  count = var.enable_az_specific == true ? length(var.subnets_id) : 1
   allocation_id = aws_eip.nat_ip.*.id[count.index]
   subnet_id     = var.subnets_id[count.index]
   tags = merge(
