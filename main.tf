e046a78be828a44cd1287db7c8736f46b4105b8d
sumitmoral <146178785+sumitmoral@users.noreply.github.com>
2023-10-23 11:04:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,33 @@
+# resource "aws_eip" "nat_ip" {
+#   vpc = true
+# }
+
+# resource "aws_nat_gateway" "nat-gw" {
+#   allocation_id = aws_eip.nat_ip.id
+#   subnet_id     = element(var.subnets_for_nat_gw,1)
+#   tags = merge(
+#     {
+#       Name = format("%s", var.nat_name)
+#     },
+#     var.tags,
+#   )
+# }
+
+
 resource "aws_eip" "nat_ip" {
-  vpc = true
+  count = length(var.subnets_for_nat_gw)
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "nat-gw" {
-  allocation_id = aws_eip.nat_ip.id
-  subnet_id     = element(var.subnets_for_nat_gw,1)
+  count = length(var.subnets_for_nat_gw)
+  allocation_id = aws_eip.nat_ip[count.index].id
+  subnet_id     = element(var.subnets_for_nat_gw, count.index) #var.subnets_for_nat_gw[count.index]
   tags = merge(
     {
-      Name = format("%s", var.nat_name)
+      Name = format("%s-%d", var.nat_name,count.index+1)
     },
     var.tags,
   )
 }
+
